9dbe5b3aa4a5bb35ad6be1cc63f90ff73a1eba38
Juan Pablo <jpwnoise@outlook.com>
2026-03-03 15:08:59 -0600

    de nuevo

---
+++ app/layout.tsx
@@ -0,0 +1,60 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Waver | Music Producer",
+  description: "Professional hip hop, trap and dark beats by Waver.",
+
+  openGraph: {
+    title: "Waver | Music Producer",
+    description: "Professional hip hop, trap and dark beats by Waver.",
+    url: "https://musicproducer-vercel.vercel.app/",
+    siteName: "Waver Beats",
+    images: [
+      {
+        url: "https://musicproducer-vercel.vercel.app/boombap.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "es_MX",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Waver | Music Producer",
+    description: "Professional hip hop, trap and dark beats by Waver.",
+    images: ["https://musicproducer-vercel.vercel.app/preview.jpg"],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <Analytics></Analytics>
+      </body>
+    </html>
+  );
+}
